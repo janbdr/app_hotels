6b0c35ece467d0c8170019963716f413ff0ad25d
janbdr <janbdr@gmail.com>
2015-04-27 02:22:22 +0530

    added sample data

--- database/migrations/2015_04_24_102700_insert_hotels.php
+++ database/migrations/2015_04_24_102700_insert_hotels.php
@@ -14,7 +14,24 @@ class InsertHotels extends Migration
     {
         //
         DB::table('hotels')->insert(
-            ['name' => 'Hotel 1', 'address' => 'address 1','location' => 'location 1',]
+		[
+			['name' => 'Hotel 1', 'address' => 'address 1','location' => 'location 1'],
+			['name' => 'Hotel 2', 'address' => 'address 2','location' => 'location 2'],
+			['name' => 'Hotel 3', 'address' => 'address 3','location' => 'location 3'],
+			['name' => 'Hotel 4', 'address' => 'address 4','location' => 'location 4'],
+			['name' => 'Hotel 5', 'address' => 'address 5','location' => 'location 5'],
+			['name' => 'Hotel 6', 'address' => 'address 6','location' => 'location 6'],
+			['name' => 'Hotel 7', 'address' => 'address 7','location' => 'location 7'],
+			['name' => 'Hotel 8', 'address' => 'address 8','location' => 'location 8'],
+			['name' => 'Hotel 9', 'address' => 'address 9','location' => 'location 9'],
+			['name' => 'Hotel 10', 'address' => 'address 10','location' => 'location 10'],
+			['name' => 'Hotel 11', 'address' => 'address 11','location' => 'location 11'],
+			['name' => 'Hotel 12', 'address' => 'address 12','location' => 'location 12']
+			
+		]
+            
+			
+			
         );
     }
 
